bfb8f6272a40cacbe315011907b57c7ce13a4602
dimtabu <dima.taburetkin@gmail.com>
2022-03-09 12:29:30 +0300

    adding exports: addKnownCtor, isKnownCtorInstance

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-export { knownCtors, isKnownCtor, removeKnownCtor } from './knownCtors.js';
+export { knownCtors, isKnownCtor, removeKnownCtor, addKnownCtor, isKnownCtorInstance } from './knownCtors.js';
 export { builderConfig } from './builderConfig.js'
 export { build } from './build.js';
